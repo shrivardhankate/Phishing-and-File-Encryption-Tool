e59f89477b3416029958f9d8e367da47f8be59b9
kateshrivardhan <kateshrivardhan@gmail.com>
2026-02-28 22:33:19 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,144 @@
+from flask import Flask, render_template, request
+import re
+from urllib.parse import urlparse
+import ipaddress
+from cryptography.fernet import Fernet
+import os 
+from flask import Flask, render_template, request, send_file
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
+ENCRYPTED_FOLDER = os.path.join(BASE_DIR, "encrypted_files")
+
+app = Flask(__name__)
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    url = request.form['url']
+    score = 0
+    reasons = []
+
+    # Check HTTPS
+    if not url.startswith("https"):
+        score += 20
+        reasons.append("URL does not use HTTPS.")
+
+    # URL length
+    if len(url) > 75:
+        score += 15
+        reasons.append("URL length is suspiciously long.")
+    
+    #check for IP address in URL
+    try:
+        ipaddress.ip_address(urlparse(url).netloc)
+        score += 25
+        reasons.append("URL contains an IP address instead of a domain name.")
+    except ValueError:
+        pass
+    
+    #suspicious TDL 
+    suspicious_tlds = [".tk", ".ml", ".ga", ".cf"]
+    for tld in suspicious_tlds:
+        if url.lower().endswith(tld):
+            score += 20
+            reasons.append(f"URL uses suspicious TLD: {tld}")
+            break
+
+    # Suspicious symbols
+    if "@" in url:
+        score += 20
+        reasons.append("URL contains '@' symbol.")
+
+    if "-" in url:
+        score += 10
+        reasons.append("URL contains hyphen (-).")
+
+    # Multiple subdomains
+    domain = urlparse(url).netloc
+    if domain.count(".") > 2:
+        score += 15
+        reasons.append("URL contains multiple subdomains.")
+
+    # Suspicious keywords
+    suspicious_keywords = ["login", "verify", "secure", "update", "bank"]
+    for word in suspicious_keywords:
+        if word in url.lower():
+            score += 10
+            reasons.append(f"URL contains suspicious keyword: {word}")
+            break
+
+    # Risk level
+    if score <= 20:
+        risk = "Low Risk"
+    elif score <= 50:
+        risk = "Medium Risk"
+    else:
+        risk = "High Risk"
+    return render_template("result.html", url=url, score=score, risk=risk, reasons=reasons)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/encryption')
+def encryption():
+    return render_template('encryption.html')
+
+
+
+@app.route('/phishing')
+def phishing():
+    return render_template('phishing.html')
+
+@app.route('/encrypt', methods=['POST'])
+def encrypt_file():
+    file = request.files['file']
+
+    if file:
+        os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+        os.makedirs(ENCRYPTED_FOLDER, exist_ok=True)
+
+        # Save original file
+        original_path = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(original_path)
+
+        # Generate key
+        key = Fernet.generate_key()
+        cipher = Fernet(key)
+
+        # Read original file
+        with open(original_path, "rb") as f:
+            file_data = f.read()
+
+        # Encrypt
+        encrypted_data = cipher.encrypt(file_data)
+
+        encrypted_filename = file.filename + ".enc"
+        encrypted_path = os.path.join(ENCRYPTED_FOLDER, encrypted_filename)
+
+        with open(encrypted_path, "wb") as f:
+            f.write(encrypted_data)
+
+        return f"""
+        <h3>File Encrypted Successfully</h3>
+        <p><strong>Encryption Key:</strong> {key.decode()}</p>
+        <p><b>Save this key securely. It is required for decryption.</b></p>
+
+        <br>
+        <a href="/download/{encrypted_filename}">Download Encrypted File</a>
+
+        <br><br>
+        <a href="/">Back to Home</a>
+        """
+        
+    return "No file uploaded."
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    file_path = os.path.join(ENCRYPTED_FOLDER, filename)
+    print("Downloading:", file_path)
+    return send_file(file_path, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
